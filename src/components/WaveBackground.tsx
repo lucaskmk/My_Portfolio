db4186846f081c03b5451989083d51f4b9bbff06
LKenji <54600289+lucaskmk@users.noreply.github.com>
2026-06-24 00:19:25 -0300

    animacoes 1/4

--- src/components/WaveBackground.tsx
+++ src/components/WaveBackground.tsx
@@ -19,7 +19,7 @@ export default function WaveBackground() {
           <svg
             viewBox="0 0 2880 160"
             preserveAspectRatio="none"
-            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws3 9s linear infinite' }}
+            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws3 36s linear infinite' }}
           >
             <path
               d="M0,60 C480,140 960,0 1440,60 C1920,140 2400,0 2880,60 L2880,160 L0,160 Z"
@@ -33,7 +33,7 @@ export default function WaveBackground() {
           <svg
             viewBox="0 0 2880 160"
             preserveAspectRatio="none"
-            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws1 6s linear infinite' }}
+            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws1 24s linear infinite' }}
           >
             <path
               d="M0,80 C360,10 1080,150 1440,80 C1800,10 2520,150 2880,80 L2880,160 L0,160 Z"
@@ -47,7 +47,7 @@ export default function WaveBackground() {
           <svg
             viewBox="0 0 2880 160"
             preserveAspectRatio="none"
-            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws2 4s linear infinite' }}
+            style={{ position: 'absolute', bottom: 0, left: 0, width: '200%', height: '100%', animation: 'ws2 16s linear infinite' }}
           >
             <path
               d="M0,90 C240,20 720,160 1440,90 C1680,20 2160,160 2880,90 L2880,160 L0,160 Z"
